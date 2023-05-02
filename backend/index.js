c3168d9cd03007dfc508dbef6c6b39ff9ba2e6c9
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-05-02 11:10:45 -0300

    feat: update post user function

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,21 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+const axios = require("axios");
+
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "328640ac-f266-42ac-8a44-9999cad9a7cb" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
-app.listen(3001);
+app.listen(7000, () => console.log("Server started"));
